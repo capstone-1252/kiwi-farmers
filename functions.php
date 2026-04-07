aa148290217fbdae0a2498f2d9317cb90ff2f60b
Rodney Russell <rodney.russell2005@gmail.com>
2026-04-07 11:14:29 -0600

    fixed an issue in functions.php where only the 1.0 version of style.css was read, preventing other styles from being read.

--- functions.php
+++ functions.php
@@ -1,11 +1,14 @@
 <?php
 
 function mytheme_enqueue_styles() {
-    // Enqueue the main theme stylesheet (style.css in the theme root)
-    wp_enqueue_style( 'mytheme-main-style', get_stylesheet_uri() );
-
-    // Enqueue an additional custom stylesheet (e.g., in a 'css' subfolder)
-    wp_enqueue_style( 'mytheme-custom-style', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all' );
+    $style_path = get_stylesheet_directory() . '/style.css';
+    $version = file_exists($style_path) ? filemtime($style_path) : '1.0';
+    wp_enqueue_style( 
+        'mytheme-main-style', 
+        get_stylesheet_uri(), 
+        array(), 
+        $version 
+    );
 }
 add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_styles' );
 
